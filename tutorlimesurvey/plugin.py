5e9edacbca2e9d816121a14e2c361c43c3b23da4
Luis Felipe Castano <felipe.castano@edunext.co>
2025-08-28 16:14:45 -0500

    chore: deprecated pkg_resources

--- tutorlimesurvey/plugin.py
+++ tutorlimesurvey/plugin.py
@@ -5,7 +5,7 @@ import os.path
 from glob import glob
 
 import click
-import pkg_resources
+import importlib.resources
 from tutor import hooks
 
 from .__about__ import __version__
@@ -84,9 +84,7 @@ MY_INIT_TASKS: list[tuple[str, tuple[str, ...]]] = [
 # and add it to the CLI_DO_INIT_TASKS filter, which tells Tutor to
 # run it as part of the `init` job.
 for service, template_path in MY_INIT_TASKS:
-    full_path: str = pkg_resources.resource_filename(
-        "tutorlimesurvey", os.path.join("templates", *template_path)
-    )
+    full_path: str = importlib.resources.files("tutorlimesurvey") / os.path.join("templates", *template_path)
     with open(full_path, encoding="utf-8") as init_task_file:
         init_task: str = init_task_file.read()
     hooks.Filters.CLI_DO_INIT_TASKS.add_item((service, init_task))
@@ -152,7 +150,7 @@ hooks.Filters.IMAGES_PUSH.add_items(
 hooks.Filters.ENV_TEMPLATE_ROOTS.add_items(
     # Root paths for template files, relative to the project root.
     [
-        pkg_resources.resource_filename("tutorlimesurvey", "templates"),
+        importlib.resources.files("tutorlimesurvey") / "templates",
     ]
 )
 
@@ -179,7 +177,7 @@ hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
 # apply a patch based on the file's name and contents.
 for path in glob(
     os.path.join(
-        pkg_resources.resource_filename("tutorlimesurvey", "patches"),
+        importlib.resources.files("tutorlimesurvey") / "patches",
         "*",
     )
 ):
